5f5098c8f19c75452b6fee50977fe547a70958fa
Zahria Shannon <shannon.zahria@gmail.com>
2020-07-06 13:49:51 -0400

    adds all playlist

--- client/src/components/zodiacplaylist/playlistId.js
+++ client/src/components/zodiacplaylist/playlistId.js
@@ -6,7 +6,47 @@ const playlistId = [
     {
         name: 'taurus',
         id: '37i9dQZF1DXbCgDGG5xQtb'
-    }
+    },
+    {
+        name: 'gemini',
+        id: '37i9dQZF1DWWVULl5wUsL9'
+    },
+    {
+        name: 'cancer',
+        id: '37i9dQZF1DXaeX3MJpiD4U'
+    },
+    {
+        name: 'leo',
+        id: '37i9dQZF1DX7cvHpkIJFt2'
+    },
+    {
+        name: 'virgo',
+        id: '37i9dQZF1DX6PdsVYbP4rI'
+    },
+    {
+        name: 'libra',
+        id: '37i9dQZF1DXco4NYQOMLiT'
+    },
+    {
+        name: 'scorpio',
+        id: '37i9dQZF1DX0YZgrwmizcR'
+    },
+    {
+        name: 'sag',
+        id: '37i9dQZF1DX93MXPufCcuk'
+    },
+    {
+        name: 'capricorn',
+        id: '37i9dQZF1DX2rcqmLx0nK4'
+    },
+    {
+        name: 'aquarius',
+        id: '37i9dQZF1DX7F9VDRJOFhw'
+    },
+    {
+        name: 'pisces',
+        id: '37i9dQZF1DWX0EDWtabVRv'
+    },
 
 ]
 
