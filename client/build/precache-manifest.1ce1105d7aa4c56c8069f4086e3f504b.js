ad0077d3dd37408a95c407a33ed78178ae52299e
Zahria Shannon <shannon.zahria@gmail.com>
2020-08-02 17:48:52 -0400

    adds changes

---
+++ client/build/precache-manifest.1ce1105d7aa4c56c8069f4086e3f504b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b7a11516a0a2e9594b33915083a9ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6abf4f540f74ba7e1747",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "16106579bfa58c69cf7e",
+    "url": "/static/css/main.3828015a.chunk.css"
+  },
+  {
+    "revision": "6abf4f540f74ba7e1747",
+    "url": "/static/js/2.2dda96b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2dda96b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16106579bfa58c69cf7e",
+    "url": "/static/js/main.dcc39047.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ef35d39b5adb730befe9dc038d281630",
+    "url": "/static/media/vintage.ef35d39b.jpg"
+  }
+]);
